bba7a35f416ad7c8759cc0c2a472a0ca03060469
artiomEBS <69451928+artiomEBS@users.noreply.github.com>
2021-06-28 12:09:12 +0300

    Removed blank=True from duration in model TileLog

--- apps/journal/models.py
+++ apps/journal/models.py
@@ -7,7 +7,7 @@ from apps.tasks.models import Task
 class TimeLog(models.Model):
     task = models.ForeignKey(Task, on_delete=CASCADE, related_name='time_logs')
     started_at = models.DateTimeField(auto_now_add=True)
-    duration = models.DurationField(null=True, blank=True)
+    duration = models.DurationField(null=True)
 
     def __str__(self):
         return f'{self.task}, {self.started_at}, {self.duration}'
